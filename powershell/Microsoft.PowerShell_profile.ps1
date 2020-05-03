757c8cc1345c657b7329ee411eb66eb7f50fcb9e
Fabio <fabio.sangregorio@gmail.com>
2020-05-03 12:27:31 +0200

    Add PSReadline to Profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,10 @@ Set-Theme Agnoster
 $DefaultUser = 'fabio'
 $env:PYTHONIOENCODING="utf-8"
 iex "$(thefuck --alias)"
+
+## Autocomplete history
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
